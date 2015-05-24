3844e28306aa8edae5ef5023fb5cf4e3e3a0b5cc
David Norden <david.norden@xero.com>
2015-05-24 12:56:36 +1200

    working on normalizing the volume

---
+++ SpotifyCube/Spotify.Cube.Library.Unit.Tests/About_smartangles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Spotify.Cube.Library.Unit.Tests
+{
+    public class About_smartangles
+    {
+         
+
+        [Theory]
+        [InlineData(0, 360, 0, 100, 0, 0)]
+        [InlineData(0, 360, 0, 100, 360, 100)]
+        [InlineData(0, 360, 0, 100, 3.6, 1)]
+        [InlineData(0, 360, 0, 100, 180, 50)]
+        public void for_an_input_we_get_the_right_output(double minInputValue, double maxInputValue, double minOutputValue, double maxOutputValue,double inputValue, double expectedOutputValue)
+        {
+            var angle = new SmartAngle(minInputValue, maxInputValue, minOutputValue, maxOutputValue);
+            
+            angle.LockWith(0,0);
+
+            angle.ChangeWith(inputValue);
+
+            Assert.True(angle.OutputValue == expectedOutputValue);
+        }
+
+
+    }
+}
---
+++ SpotifyCube/Spotify.Cube.Library/SmartAngle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Spotify.Cube.Library
+{
+    public class SmartAngle
+    {
+        public double MaxOutputValue { get; set; }
+
+        public double MinOutputValue { get; set; }
+
+        public double MaxInputValue { get; set; }
+
+        public double MinInputValue{ get; set; }
+
+        public double OutputValue { get; set; }
+
+        public double LockedOuputValue { get; set; }
+
+        public double LockedInputValue  { get; set; }
+
+        public SmartAngle(double minInputValue, double maxInputValue, double minOutputValue, double maxOutputValue)
+        {
+            LockWith(0, 0);
+
+            MinInputValue = minInputValue;
+            
+            MaxInputValue = maxInputValue;
+
+            MinOutputValue = minOutputValue;
+            
+            MaxOutputValue = maxOutputValue;
+            
+        }
+
+        public void LockWith(double volume, double cubeAngle)
+        {
+            LockedInputValue = volume;
+
+            LockedOuputValue = cubeAngle;
+
+        }
+
+        public void ChangeWith(double inputvalue)
+        {
+
+            var pctOfMaxRange = (double)1 / (double)MaxInputValue;
+
+            var normalizedInputValue= NormalizeInputValue(inputvalue);
+
+            double value = normalizedInputValue * pctOfMaxRange;
+
+            var outputValue = Math.Round(value * MaxOutputValue);
+
+            var normalizedOutputValue = NormalizeOuputValue(outputValue);
+
+            OutputValue = normalizedOutputValue;
+
+        }
+
+        private double NormalizeInputValue(double inputvalue)
+        {
+            return inputvalue;
+        }
+
+        private double NormalizeOuputValue(double outputvalue)
+        {
+            return outputvalue;
+        }
+
+    }
+
+
+    
+}
